45f42bd1a33cb91477bc6f05a0fd11fdba394589
ObiWanCanOweMe <andrew@kener.org>
2015-07-02 10:48:52 -0400

    Update usbredir.rb

--- usbredir.rb
+++ usbredir.rb
@@ -1,6 +1,6 @@
 class Usbredir < Formula
   homepage "http://www.spice-space.org/home.html"
-  url "http://www.spice-space.org/download/usbredir/usbredir-0.7.tar.bz2"
+  url "http://pkgs.fedoraproject.org/repo/pkgs/usbredir/usbredir-0.7.tar.bz2/bc096f160e4ebb848c3a0f0fbf9500fc/usbredir-0.7.tar.bz2"
   sha256 "0a63a0712b5dc62be9cca44f97270fea5d1ec1fe7dde0c11dc74a01c8e2006aa"
 
   depends_on "pkg-config" => :build
