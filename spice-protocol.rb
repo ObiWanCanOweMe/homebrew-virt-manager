23936c1a18e236260f69335e84a4ced7d512fe19
ObiWanCanOweMe <andrew@kener.org>
2015-07-02 10:51:28 -0400

    Update spice-protocol.rb

--- spice-protocol.rb
+++ spice-protocol.rb
@@ -1,6 +1,6 @@
 class SpiceProtocol < Formula
   homepage "http://www.spice-space.org/"
-  url "http://www.spice-space.org/download/releases/spice-protocol-0.12.7.tar.bz2"
+  url "http://pkgs.fedoraproject.org/repo/pkgs/spice-protocol/spice-protocol-0.12.7.tar.bz2/799707321e88df4ecd17c4ccb0cc4ffe/spice-protocol-0.12.7.tar.bz2"
   version "0.12.7"
   sha256 "96c6cb73361816ed19bfd8536f65d4547da35709aa00af1380e650c1174916c2"
 
